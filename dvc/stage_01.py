c9509389b32481860aa190c3c56c042bd63f551b
manish72 <manishguru72@gmail.com>
2023-06-25 17:39:02 +0530

    basic dvc implemented

---
+++ dvc/stage_01.py
@@ -0,0 +1,4 @@
+
+
+with open("artifact.txt","w") as f:
+    f.write("My name is manish stage one is done")
---
+++ dvc/stage_02.py
@@ -0,0 +1,6 @@
+with open("artifact.txt","r") as f:
+    text = f.read()
+
+print(text)
+print("It is a end of stage 2")
+print("I am going to add stage 3")
